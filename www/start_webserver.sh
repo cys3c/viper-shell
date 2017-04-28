c0321c4b87d771a83ddd67fe34abc7ab76c2c180
root <jinverar@gmail.com>
2017-04-28 02:08:37 -0400

    java app

---
+++ www/start_webserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### Background
+echo "[*] - Starting the webserver reactor"
+echo "[*] - The webserver is listening on 127.0.0.1:8080"
+echo "[*] - The reactor is running"
+twistd web --path html/.
